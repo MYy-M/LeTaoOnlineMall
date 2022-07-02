1ea45767faacc22d6aac7f4aef6e017d6d91898b
Russell-Gustuv <79741284+Russell-Gustuv@users.noreply.github.com>
2022-07-02 09:56:17 +0800

    后台页面模板（不依赖vue-cli）

--- frontend/admin/mock/utils.js
+++ frontend/admin/mock/utils.js
@@ -1,25 +1,14 @@
-/**
- * @param {string} url
- * @returns {Object}
- */
-function param2Obj(url) {
-  const search = decodeURIComponent(url.split('?')[1]).replace(/\+/g, ' ')
+export function param2Obj(url) {
+  const search = url.split('?')[1]
   if (!search) {
     return {}
   }
-  const obj = {}
-  const searchArr = search.split('&')
-  searchArr.forEach(v => {
-    const index = v.indexOf('=')
-    if (index !== -1) {
-      const name = v.substring(0, index)
-      const val = v.substring(index + 1, v.length)
-      obj[name] = val
-    }
-  })
-  return obj
-}
-
-module.exports = {
-  param2Obj
+  return JSON.parse(
+    '{"' +
+      decodeURIComponent(search)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"') +
+      '"}'
+  )
 }
