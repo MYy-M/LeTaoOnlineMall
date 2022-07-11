faca0c4d41305ee1d427054a713755b0e8124fa8
Arthur-yanyawei <1737158173@qq.com>
2022-07-11 09:58:41 +0800

    Add files via upload

--- mall/src/main/java/com/letao/mall/controller/CommodityController.java
+++ mall/src/main/java/com/letao/mall/controller/CommodityController.java
@@ -3,8 +3,12 @@ package com.letao.mall.controller;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.letao.mall.dao.entity.Commodity;
 import com.letao.mall.service.CommodityService;
+import com.letao.mall.vo.ErrorCode;
+import com.letao.mall.vo.Result;
+import com.letao.mall.vo.param.PageParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,59 +33,72 @@ public class CommodityController {
 
 //    //根据评分商品排序
 //    @GetMapping("/getlist/score")
-//    public List<Commodity> showCommodityListByScore(
+//    public Result showCommodityListByScore(
 //            String cmname
 //    ){
 //        QueryWrapper<Commodity> wrapper = new QueryWrapper<>();
 //        wrapper.eq("cname", cmname);
+//        if(wrapper == null)
+//            return Result.fail(ErrorCode.COMMODITY_NOT_EXIST.getCode(), ErrorCode.COMMODITY_NOT_EXIST.getMsg());
 //        wrapper.orderByDesc("score");
 //        List<Commodity> commodityList = commodityService.list(wrapper);
-//        return commodityList;
+//        return Result.success(commodityList);
 //    }
 
-    //根据销量商品排序
-    @GetMapping("/getlist/<sales")
-    public List<Commodity> showCommodityListBySales(
+    //根据销量商品排序分页展示
+    @GetMapping("/getlist/salesdesc/{pnum}")
+    public Result showCommodityListBySales(
+            @PathVariable("pnum") PageParam pageParam,
             String cmname
     ){
         QueryWrapper<Commodity> wrapper = new QueryWrapper<>();
         wrapper.like("cname", cmname);
+        if(wrapper == null)
+            return Result.fail(ErrorCode.COMMODITY_NOT_EXIST.getCode(), ErrorCode.COMMODITY_NOT_EXIST.getMsg());
         wrapper.orderByDesc("sales");
-        List<Commodity> commodityList = commodityService.list(wrapper);
-        return commodityList;
+        Page<Commodity> commodityPage = new Page<>(pageParam.getPage(),pageParam.getPageSize());
+        return Result.success(commodityService.page(commodityPage,wrapper));
     }
 
-    //输入商品名字根据价格商品降序排序
+    //输入商品名字根据价格商品降序排序分页展示
     @RequestMapping("/getlist/pricedesc")
-    public List<Commodity> showCommodityListByPriceDesc(
+    public Result showCommodityListByPriceDesc(
+            @PathVariable("pnum") PageParam pageParam,
             String cmname
     ){
         QueryWrapper<Commodity> wrapper = new QueryWrapper<>();
         wrapper.like("cname", cmname);
+        if(wrapper == null)
+            return Result.fail(ErrorCode.COMMODITY_NOT_EXIST.getCode(), ErrorCode.COMMODITY_NOT_EXIST.getMsg());
         wrapper.orderByDesc("cprice");
-        List<Commodity> commodityList = commodityService.list(wrapper);
-        return commodityList;
+        Page<Commodity> commodityPage = new Page<>(pageParam.getPage(),pageParam.getPageSize());
+        return Result.success(commodityService.page(commodityPage,wrapper));
     }
 
-    //输入商品名字根据价格商品升序排序
+    //输入商品名字根据价格商品升序排序分页展示
     @RequestMapping("/getlist/priceasc")
-    public List<Commodity> showCommodityListByPriceAsc(
+    public Result showCommodityListByPriceAsc(
+            @PathVariable("pnum") PageParam pageParam,
             String cmname
     ){
         QueryWrapper<Commodity> wrapper = new QueryWrapper<>();
         wrapper.like("cname", cmname);
+        if(wrapper == null)
+            return Result.fail(ErrorCode.COMMODITY_NOT_EXIST.getCode(), ErrorCode.COMMODITY_NOT_EXIST.getMsg());
         wrapper.orderByAsc("cprice");
-        List<Commodity> commodityList = commodityService.list(wrapper);
-        return commodityList;
+        Page<Commodity> commodityPage = new Page<>(pageParam.getPage(),pageParam.getPageSize());
+        return Result.success(commodityService.page(commodityPage,wrapper));
     }
 
 
     //查看商品细节
     @GetMapping("/getDetail")
-    public String getCommodityDetail(
+    public Result getCommodityDetail(
             Commodity commodity
     ){
-        return commodityService.getById(commodity).getCdetail();
+        if(commodityService.getById(commodity).getCdetail() == null )
+            return Result.fail(ErrorCode.DETAIL_NOT_EXIST.getCode(), ErrorCode.DETAIL_NOT_EXIST.getMsg());
+        return Result.success(commodityService.getById(commodity).getCdetail());
     }
 }
 
