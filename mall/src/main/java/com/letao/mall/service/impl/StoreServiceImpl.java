7c7fd033b8d0fe6fded869395068f3bc60275d55
Arthur-yanyawei <1737158173@qq.com>
2022-07-11 10:08:15 +0800

    Add files via upload

--- mall/src/main/java/com/letao/mall/service/impl/StoreServiceImpl.java
+++ mall/src/main/java/com/letao/mall/service/impl/StoreServiceImpl.java
@@ -1,9 +1,14 @@
 package com.letao.mall.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.letao.mall.dao.entity.Store;
 import com.letao.mall.dao.mapper.StoreMapper;
 import com.letao.mall.service.StoreService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.letao.mall.vo.Result;
+import com.letao.mall.vo.param.PageParam;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +22,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class StoreServiceImpl extends ServiceImpl<StoreMapper, Store> implements StoreService {
 
+    /**
+     * 查询个人的购物车
+     * @param pageParam
+     * @return
+     */
+    @Override
+    public Result showStore(PageParam pageParam){
+        Page<Store> cartPage = new Page<>(pageParam.getPage(),pageParam.getPageSize());
+        LambdaQueryWrapper<Store> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Store::getSid,pageParam.getUid());
+        return Result.success(this.page(cartPage,queryWrapper));
+    }
 }
