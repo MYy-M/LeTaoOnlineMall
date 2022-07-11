b44248836bed5ea2c63d8c02ed4544241d83e99e
Arthur-yanyawei <1737158173@qq.com>
2022-07-11 10:05:13 +0800

    Add files via upload

--- mall/src/main/java/com/letao/mall/vo/ErrorCode.java
+++ mall/src/main/java/com/letao/mall/vo/ErrorCode.java
@@ -4,23 +4,19 @@ public enum  ErrorCode {
     PARAMS_ERROR(10001,"参数有误"),
     ACCOUNT_PWD_NOT_EXIST(10002,"用户名或密码不存在"),
     TOKEN_ERROR(10003,"token不合法"),
-
     ACCOUNT_EXIST(10004,"账号已存在"),
     ACCOUNT_NOT_EXIST(10005,"账号不存在"),
-
     ADD_ERROR(20001,"添加失败"),
-
     DELETE_ERROR(20002,"删除失败"),
-
     MODIFY_ERROR(20003,"修改失败"),
-
     SEARCH_ERROR(20004,"查询失败"),
-
+    STORE_EXIST(30001,"门店已存在"),
+    STORE_NOT_EXIST(30002,"门店不存在"),
+    COMMODITY_NOT_EXIST(30003,"您想要的商品还未上架,小乐已为您记录"),
+    DETAIL_NOT_EXIST(30004,"加载失败"),
     NO_PERMISSION(70001,"无访问权限"),
     SESSION_TIME_OUT(90001,"会话超时"),
     LOGIN_ING(90002,"已登录"),
-
-
     NO_LOGIN(90003,"未登录"),;
 
     private int code;
