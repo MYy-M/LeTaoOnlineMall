1cbedc875975ae42eecd95215fdbe191f1694046
Wuwf <2811525987@qq.com>
2022-07-12 16:50:50 +0800

    。

--- mall/src/main/java/com/letao/mall/controller/Consumer/CLtOrderController.java
+++ mall/src/main/java/com/letao/mall/controller/Consumer/CLtOrderController.java
@@ -1,14 +1,18 @@
 package com.letao.mall.controller.Consumer;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.letao.mall.dao.entity.LtOrder;
+import com.letao.mall.dao.entity.Orderitem;
 import com.letao.mall.vo.ErrorCode;
 import com.letao.mall.vo.Result;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.letao.mall.vo.param.OrderParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 /**
  * <p>
@@ -31,10 +35,20 @@ public class CLtOrderController {
         }
     }
 
-    @GetMapping("/addOrder")
-    public Result addOrder(){
+    @RequestMapping("/submitOrder")
+    public Boolean submitOrder(String orderParam){
+        JSONObject jsonObject= JSON.parseObject(orderParam);
 
-        return null;
+        LtOrder ltOrder=jsonObject.getObject("ltOrder",LtOrder.class);
+        JSONArray jsonArray=jsonObject.getJSONArray("itemList");
+        ArrayList<Orderitem> itemList=new ArrayList<>();
+        for(int i=0;i<jsonArray.size();i++){
+            itemList.add(jsonArray.getObject(i,Orderitem.class));
+        }
+        System.out.println(ltOrder);
+        System.out.println("-----");
+        System.out.println(itemList);
+        return true;
     }
 
 
