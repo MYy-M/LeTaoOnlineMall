039f082f6123573a71f5ebec37b66dd0ec7cd902
Russell-Gustuv <79741284+Russell-Gustuv@users.noreply.github.com>
2022-07-18 00:13:08 +0800

    修改

--- mall/src/main/java/com/letao/mall/controller/admin/CategoryController.java
+++ mall/src/main/java/com/letao/mall/controller/admin/CategoryController.java
@@ -86,84 +86,7 @@ public class CategoryController {
      * @param param
      * @return
      */
-    @RequestMapping("/addAttributeKey")
-    public Result addAttributeKey(@RequestBody AttributeParam param){
-        long categoryId= param.getId();
-        String key= param.getAttributeKey();
-        String values= param.getAttributeValue().replaceAll(" ","");
 
-        if(categoryId==0||key==null||values==null){
-            return Result.fail(ErrorCode.PARAMS_ERROR.getCode(), ErrorCode.PARAMS_ERROR.getMsg());
-        }
-        AttributeKey attributeKey=new AttributeKey();
-        attributeKey.setCategoryId(categoryId);
-        attributeKey.setAttributeName(key);
-
-        //属性名在该分类中已存在就不能添加
-        if(aks.getSameKey(categoryId,key)!=null){
-            return Result.fail(ErrorCode.KEY_EXIST.getCode(), ErrorCode.KEY_EXIST.getMsg());
-        }
-        if(!aks.save(attributeKey)){
-            /*System.out.println("111");*/
-            return Result.fail(ErrorCode.ADD_ERROR.getCode(), ErrorCode.ADD_ERROR.getMsg());
-        }
-        long keyId=attributeKey.getId();
-
-        //中文分号
-        String[] value=values.replaceAll(";","；").split("；");
-        ArrayList<AttributeValue> list=new ArrayList<>();
-        for(String v:value){
-            AttributeValue attributeValue=new AttributeValue();
-            attributeValue.setAttributeValue(v);
-            attributeValue.setAttributeId(keyId);
-            list.add(attributeValue);
-        }
-        if(avs.saveBatch(list)){
-            return Result.success(true);
-        }else{
-            /*System.out.println("222");*/
-            return Result.fail(ErrorCode.ADD_VALUE_ERROR.getCode(), ErrorCode.ADD_VALUE_ERROR.getMsg());
-        }
-    }
-
-    /**
-     * 修改属性，输入属性key的id，key的name，key的value
-     * @param param
-     * @return
-     */
-    @RequestMapping("/modifyAttribute")
-    public Result modifyAttribute(@RequestBody AttributeParam param){
-        long id= param.getId();
-        String key= param.getAttributeKey();
-        String values= param.getAttributeValue().replaceAll(" ","");
-        if(id==0||key==null||values==null){
-            return Result.fail(ErrorCode.PARAMS_ERROR.getCode(), ErrorCode.PARAMS_ERROR.getMsg());
-        }
-        AttributeKey attributeKey=new AttributeKey();
-        attributeKey.setId(id);
-        attributeKey.setAttributeName(key);
-        if(!aks.updateById(attributeKey)){
-            return Result.fail(ErrorCode.MODIFY_ERROR.getCode(), ErrorCode.MODIFY_ERROR.getMsg());
-        }
-
-        avs.deleteAttributeValue(id);
-
-        String[] value=values.split("；");
-        ArrayList<AttributeValue> list=new ArrayList<>();
-        for(String v:value){
-            AttributeValue attributeValue=new AttributeValue();
-            attributeValue.setAttributeValue(v);
-            attributeValue.setAttributeId(id);
-            list.add(attributeValue);
-        }
-        if(avs.saveBatch(list)){
-            return Result.success(true);
-        }else{
-            /*System.out.println("222");*/
-            return Result.fail(ErrorCode.MODIFY_ERROR.getCode(), ErrorCode.MODIFY_ERROR.getMsg());
-        }
-
-    }
 
     /**
      * 删除类别
@@ -303,15 +226,7 @@ public class CategoryController {
         return list.isEmpty()?Result.fail(ErrorCode.SEARCH_ERROR.getCode(), ErrorCode.SEARCH_ERROR.getMsg()):Result.success(list);
     }
 
-    @PostMapping("/addAttributeValue")
-    public Result addAttributeValue(@RequestBody AttributeValue attributeValue){
-        return avs.save(attributeValue)?Result.success(new Boolean(true)):Result.fail(ErrorCode.ADD_ERROR.getCode(), ErrorCode.ADD_ERROR.getMsg());
-    }
 
-    @PostMapping("/modifyAttributeValue")
-    public Result modifyAttributeValue(@RequestBody AttributeValue attributeValue){
-        return avs.updateById(attributeValue)?Result.success(new Boolean(true)):Result.fail(ErrorCode.MODIFY_ERROR.getCode(), ErrorCode.MODIFY_ERROR.getMsg());
-    }
     @GetMapping("/deleteAttributeValue")
     public Result deleteAttributeValue(Long id){
         return avs.deleteAttributeValue(id)?Result.success(new Boolean(true)):Result.fail(ErrorCode.DELETE_ERROR.getCode(), ErrorCode.DELETE_ERROR.getMsg());
