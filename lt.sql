b181e5a75297084953ef25ecbcbbcb946a289f0c
Wuwf <2811525987@qq.com>
2022-07-18 09:43:09 +0800

    。

--- lt.sql
+++ lt.sql
@@ -1,273 +1,206 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : test
- Source Server Type    : MySQL
- Source Server Version : 80027
- Source Host           : localhost:3306
- Source Schema         : lt
+Source Server         : localhost
+Source Server Version : 80028
+Source Host           : localhost:3306
+Source Database       : lt
 
- Target Server Type    : MySQL
- Target Server Version : 80027
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 80028
+File Encoding         : 65001
 
- Date: 14/07/2022 15:20:47
+Date: 2022-07-18 09:42:23
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for admin
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin`  (
+CREATE TABLE `admin` (
   `aid` bigint NOT NULL,
   `ausername` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `apassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `alevel` int NULL DEFAULT NULL COMMENT '权限级别',
+  `alevel` int DEFAULT NULL COMMENT '权限级别',
   PRIMARY KEY (`aid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of admin
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for attribute_key
 -- ----------------------------
 DROP TABLE IF EXISTS `attribute_key`;
-CREATE TABLE `attribute_key`  (
+CREATE TABLE `attribute_key` (
   `id` bigint NOT NULL,
   `category_id` bigint NOT NULL,
   `attribute_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '属性值',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `category_id`(`category_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of attribute_key
--- ----------------------------
+  KEY `category_id` (`category_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for attribute_value
 -- ----------------------------
 DROP TABLE IF EXISTS `attribute_value`;
-CREATE TABLE `attribute_value`  (
+CREATE TABLE `attribute_value` (
   `id` bigint NOT NULL,
   `attribute_id` bigint NOT NULL,
   `attribute_value` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '属性value值',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `attribute_id`(`attribute_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of attribute_value
--- ----------------------------
+  KEY `attribute_id` (`attribute_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for cart
 -- ----------------------------
 DROP TABLE IF EXISTS `cart`;
-CREATE TABLE `cart`  (
+CREATE TABLE `cart` (
   `cart_id` bigint NOT NULL,
   `uid` bigint NOT NULL,
   `cid` bigint NOT NULL,
-  `cart_num` int NULL DEFAULT 0 COMMENT '购买数量',
-  `commodity_state` int NULL DEFAULT 1 COMMENT '\"1\"表示“可以购买”，”0“表示“商品已失效”',
+  `cart_num` int DEFAULT '0' COMMENT '购买数量',
+  `commodity_state` int DEFAULT '1' COMMENT '"1"表示“可以购买”，”0“表示“商品已失效”',
   PRIMARY KEY (`cart_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of cart
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for category
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category`  (
+CREATE TABLE `category` (
   `category_id` bigint NOT NULL AUTO_INCREMENT,
   `category_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '分类名称',
-  `parent_id` bigint NULL DEFAULT NULL COMMENT '父类id',
-  `category_level` int NULL DEFAULT NULL COMMENT '分类层级',
+  `parent_id` bigint DEFAULT NULL COMMENT '父类id',
+  `category_level` int DEFAULT NULL COMMENT '分类层级',
   PRIMARY KEY (`category_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of category
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=1548211767328321539 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for comment
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment`  (
+CREATE TABLE `comment` (
   `comment_id` bigint NOT NULL,
   `cid` bigint NOT NULL,
-  `uid` bigint NULL DEFAULT NULL,
-  `comment_data` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `uid` bigint DEFAULT NULL,
+  `comment_data` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`comment_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of comment
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for commodity
 -- ----------------------------
 DROP TABLE IF EXISTS `commodity`;
-CREATE TABLE `commodity`  (
+CREATE TABLE `commodity` (
   `cid` bigint NOT NULL AUTO_INCREMENT,
   `cname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `cprice` decimal(10, 2) NOT NULL,
-  `cnum` int NOT NULL DEFAULT 0,
-  `attribute_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '规格列表',
-  `cdetail` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '商品介绍',
+  `cprice` decimal(10,2) NOT NULL,
+  `cnum` int NOT NULL DEFAULT '0',
+  `attribute_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '规格列表',
+  `cpicture` text,
+  `cdetail` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '商品介绍',
   `category_id` bigint NOT NULL,
-  `csales` int NULL DEFAULT 0,
+  `csales` int DEFAULT '0',
   PRIMARY KEY (`cid`) USING BTREE,
-  INDEX `cid`(`cid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1546337342148505603 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of commodity
--- ----------------------------
+  KEY `cid` (`cid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1546337342148505603 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for commodity_specs
 -- ----------------------------
 DROP TABLE IF EXISTS `commodity_specs`;
-CREATE TABLE `commodity_specs`  (
+CREATE TABLE `commodity_specs` (
   `id` bigint NOT NULL,
   `cid` bigint NOT NULL,
-  `cpicture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `cpicture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `c_specs` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商品规格',
-  `c_stock` int NULL DEFAULT 0,
-  `cprice` decimal(10, 2) NOT NULL,
+  `c_stock` int DEFAULT '0',
+  `cprice` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `cid`(`cid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of commodity_specs
--- ----------------------------
+  KEY `cid` (`cid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for consumer
 -- ----------------------------
 DROP TABLE IF EXISTS `consumer`;
-CREATE TABLE `consumer`  (
+CREATE TABLE `consumer` (
   `uid` bigint NOT NULL,
   `uname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `upassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `upicture` blob NULL COMMENT '头像',
-  `umail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `uaddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户地址',
-  `uphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话号码',
-  `ulevel` int NULL DEFAULT NULL COMMENT 'vip等级',
+  `upicture` blob COMMENT '头像',
+  `umail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '邮箱',
+  `uaddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户地址',
+  `uphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '电话号码',
+  `ulevel` int DEFAULT NULL COMMENT 'vip等级',
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of consumer
--- ----------------------------
-INSERT INTO `consumer` VALUES (1546400837008486402, 'myy', '0ec2a4811272ecc15c9f2aa9fc5a0cb2', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547135940533809154, 'myy6767', 'ffa5e77b572322d3b9f02e889a51f904', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547136644254134274, 'myy1212', 'b1c4bef0597f63fb9d73e02ec3323085', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547136831970209794, 'myy3434', '24e09cf5333e78915427e4913218ff3a', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547138398383370241, 'my6767', 'b8bc1338ff24b7cc2e9c6f7d00222da9', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547145044144701441, 'myy8989', 'a34c44dcf9f892c3783b5e7cacaffe12', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547145609914368001, 'my8989', '8d707f0d003cdf36a85198451bf61145', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `consumer` VALUES (1547145844661174274, 'my5656', '4c063d6570e4b9a4caad48b547e23211', NULL, NULL, NULL, NULL, NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for lt_order
 -- ----------------------------
 DROP TABLE IF EXISTS `lt_order`;
-CREATE TABLE `lt_order`  (
+CREATE TABLE `lt_order` (
   `order_id` bigint NOT NULL,
   `uid` bigint NOT NULL,
   `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `time` datetime NOT NULL,
-  `price` decimal(10, 2) NULL DEFAULT NULL,
-  `logistic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '物流情况',
-  `order_state` int NULL DEFAULT 0 COMMENT '“0”表示未收到，“1”表示已收到',
+  `price` decimal(10,2) DEFAULT NULL,
+  `logistic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '物流情况',
+  `order_state` int DEFAULT '0' COMMENT '“0”表示未收到，“1”表示已收到',
   PRIMARY KEY (`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of lt_order
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for orderitem
 -- ----------------------------
 DROP TABLE IF EXISTS `orderitem`;
-CREATE TABLE `orderitem`  (
+CREATE TABLE `orderitem` (
   `id` bigint NOT NULL,
   `order_id` bigint NOT NULL,
   `cid` bigint NOT NULL,
-  `cnum` int NULL DEFAULT 0 COMMENT '单个商品购买数量',
-  `cprice` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品单/总价(同一件商品的总价)',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of orderitem
--- ----------------------------
+  `cnum` int DEFAULT '0' COMMENT '单个商品购买数量',
+  `cprice` decimal(10,2) DEFAULT NULL COMMENT '商品单/总价(同一件商品的总价)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for recommend
 -- ----------------------------
 DROP TABLE IF EXISTS `recommend`;
-CREATE TABLE `recommend`  (
+CREATE TABLE `recommend` (
   `rid` bigint NOT NULL AUTO_INCREMENT,
   `cid` bigint NOT NULL,
-  `cpicture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `cpicture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`rid`) USING BTREE,
-  INDEX `rid`(`rid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1546337342148505603 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of recommend
--- ----------------------------
+  KEY `rid` (`rid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1546337342148505603 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for store
 -- ----------------------------
 DROP TABLE IF EXISTS `store`;
-CREATE TABLE `store`  (
+CREATE TABLE `store` (
   `sid` bigint NOT NULL,
   `sname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `saddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `sphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话11位',
+  `sphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '电话11位',
   PRIMARY KEY (`sid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of store
--- ----------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+CREATE TABLE `user` (
   `uid` bigint NOT NULL,
   `uname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `upassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `upicture` blob NULL COMMENT '头像',
-  `umail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `uaddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户地址',
-  `uphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话号码',
-  `ulevel` int NULL DEFAULT NULL COMMENT 'vip等级',
+  `upicture` blob COMMENT '头像',
+  `umail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '邮箱',
+  `uaddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户地址',
+  `uphone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '电话号码',
+  `ulevel` int DEFAULT NULL COMMENT 'vip等级',
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-
-SET FOREIGN_KEY_CHECKS = 1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
