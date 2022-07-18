33ed8ec214861f44740eb30e2aeeaff746290849
Russell-Gustuv <79741284+Russell-Gustuv@users.noreply.github.com>
2022-07-18 19:24:46 +0800

    ..

--- frontend/admin/src/views/pms/product/components/ProductDetail.vue
+++ frontend/admin/src/views/pms/product/components/ProductDetail.vue
@@ -2,9 +2,7 @@
   <el-card class="form-container" shadow="never">
     <el-steps :active="active" finish-status="success" align-center>
       <el-step title="填写商品信息"></el-step>
-      <el-step title="填写商品促销"></el-step>
       <el-step title="填写商品属性"></el-step>
-      <el-step title="选择商品关联"></el-step>
     </el-steps>
     <product-info-detail
       v-show="showStatus[0]"
@@ -12,40 +10,21 @@
       :is-edit="isEdit"
       @nextStep="nextStep">
     </product-info-detail>
-    <product-sale-detail
-      v-show="showStatus[1]"
-      v-model="productParam"
-      :is-edit="isEdit"
-      @nextStep="nextStep"
-      @prevStep="prevStep">
-    </product-sale-detail>
     <product-attr-detail
-      v-show="showStatus[2]"
-      v-model="productParam"
-      :is-edit="isEdit"
-      @nextStep="nextStep"
-      @prevStep="prevStep">
-    </product-attr-detail>
-    <product-relation-detail
-      v-show="showStatus[3]"
+      v-show="showStatus[1]"
       v-model="productParam"
       :is-edit="isEdit"
       @prevStep="prevStep"
       @finishCommit="finishCommit">
-    </product-relation-detail>
+    </product-attr-detail>
   </el-card>
 </template>
 <script>
   import ProductInfoDetail from './ProductInfoDetail';
-  import ProductSaleDetail from './ProductSaleDetail';
   import ProductAttrDetail from './ProductAttrDetail';
-  import ProductRelationDetail from './ProductRelationDetail';
   import {createProduct,getProduct,updateProduct} from '@/api/product';
 
   const defaultProductParam = {
-    albumPics: '',
-    brandId: null,
-    brandName: '',
     deleteStatus: 0,
     description: '',
     detailDesc: '',
@@ -53,25 +32,10 @@
     detailMobileHtml: '',
     detailTitle: '',
     feightTemplateId: 0,
-    flashPromotionCount: 0,
-    flashPromotionId: 0,
-    flashPromotionPrice: 0,
-    flashPromotionSort: 0,
-    giftPoint: 0,
-    giftGrowth: 0,
     keywords: '',
-    lowStock: 0,
     name: '',
-    newStatus: 0,
     note: '',
-    originalPrice: 0,
     pic: '',
-    //会员价格{memberLevelId: 0,memberPrice: 0,memberLevelName: null}
-    memberPriceList: [],
-    //商品满减
-    productFullReductionList: [{fullPrice: 0, reducePrice: 0}],
-    //商品阶梯价格
-    productLadderList: [{count: 0,discount: 0,price: 0}],
     previewStatus: 0,
     price: 0,
     productAttributeCategoryId: null,
@@ -79,33 +43,20 @@
     productAttributeValueList: [],
     //商品sku库存信息{lowStock: 0, pic: '', price: 0, sale: 0, skuCode: '', spData: '', stock: 0}
     skuStockList: [],
-    //商品相关专题{subjectId: 0}
-    subjectProductRelationList: [],
-    //商品相关优选{prefrenceAreaId: 0}
-    prefrenceAreaProductRelationList: [],
     productCategoryId: null,
     productCategoryName: '',
     productSn: '',
-    promotionEndTime: '',
-    promotionPerLimit: 0,
-    promotionPrice: null,
-    promotionStartTime: '',
-    promotionType: 0,
     publishStatus: 0,
     recommandStatus: 0,
     sale: 0,
     serviceIds: '',
-    sort: 0,
     stock: 0,
-    subTitle: '',
-    unit: '',
     usePointLimit: 0,
     verifyStatus: 0,
-    weight: 0
   };
   export default {
     name: 'ProductDetail',
-    components: {ProductInfoDetail, ProductSaleDetail, ProductAttrDetail, ProductRelationDetail},
+    components: {ProductInfoDetail,  ProductAttrDetail},
     props: {
       isEdit: {
         type: Boolean,
@@ -116,7 +67,7 @@
       return {
         active: 0,
         productParam: Object.assign({}, defaultProductParam),
-        showStatus: [true, false, false, false]
+        showStatus: [true, false]
       }
     },
     created(){
--- frontend/admin/src/views/pms/product/components/ProductInfoDetail.vue
+++ frontend/admin/src/views/pms/product/components/ProductInfoDetail.vue
@@ -10,22 +10,6 @@
       <el-form-item label="商品名称：" prop="name">
         <el-input v-model="value.name"></el-input>
       </el-form-item>
-      <el-form-item label="副标题：" prop="subTitle">
-        <el-input v-model="value.subTitle"></el-input>
-      </el-form-item>
-      <el-form-item label="商品品牌：" prop="brandId">
-        <el-select
-          v-model="value.brandId"
-          @change="handleBrandChange"
-          placeholder="请选择品牌">
-          <el-option
-            v-for="item in brandOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
-        </el-select>
-      </el-form-item>
       <el-form-item label="商品介绍：">
         <el-input
           :autoSize="true"
@@ -33,30 +17,14 @@
           type="textarea"
           placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="商品货号：">
-        <el-input v-model="value.productSn"></el-input>
-      </el-form-item>
       <el-form-item label="商品售价：">
         <el-input v-model="value.price"></el-input>
-      </el-form-item>
-      <el-form-item label="市场价：">
-        <el-input v-model="value.originalPrice"></el-input>
       </el-form-item>
       <el-form-item label="商品库存：">
         <el-input v-model="value.stock"></el-input>
       </el-form-item>
-      <el-form-item label="计量单位：">
-        <el-input v-model="value.unit"></el-input>
-      </el-form-item>
-      <el-form-item label="商品重量：">
-        <el-input v-model="value.weight" style="width: 300px"></el-input>
-        <span style="margin-left: 20px">克</span>
-      </el-form-item>
-      <el-form-item label="排序">
-        <el-input v-model="value.sort"></el-input>
-      </el-form-item>
       <el-form-item style="text-align: center">
-        <el-button type="primary" size="medium" @click="handleNext('productInfoForm')">下一步，填写商品促销</el-button>
+        <el-button type="primary" size="medium" @click="handleNext('productInfoForm')">下一步，填写商品属性</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -81,15 +49,12 @@
         //选中商品分类的值
         selectProductCateValue: [],
         productCateOptions: [],
-        brandOptions: [],
         rules: {
           name: [
             {required: true, message: '请输入商品名称', trigger: 'blur'},
             {min: 2, max: 140, message: '长度在 2 到 140 个字符', trigger: 'blur'}
           ],
-          subTitle: [{required: true, message: '请输入商品副标题', trigger: 'blur'}],
           productCategoryId: [{required: true, message: '请选择商品分类', trigger: 'blur'}],
-          brandId: [{required: true, message: '请选择商品品牌', trigger: 'blur'}],
           description: [{required: true, message: '请输入商品介绍', trigger: 'blur'}],
           requiredProp: [{required: true, message: '该项为必填项', trigger: 'blur'}]
         }
@@ -97,7 +62,6 @@
     },
     created() {
       this.getProductCateList();
-      this.getBrandList();
     },
     computed:{
       //商品的编号
@@ -146,15 +110,6 @@
           }
         });
       },
-      getBrandList() {
-        fetchBrandList({pageNum: 1, pageSize: 100}).then(response => {
-          this.brandOptions = [];
-          let brandList = response.data.list;
-          for (let i = 0; i < brandList.length; i++) {
-            this.brandOptions.push({label: brandList[i].name, value: brandList[i].id});
-          }
-        });
-      },
       getCateNameById(id){
         let name=null;
         for(let i=0;i<this.productCateOptions.length;i++){
@@ -180,16 +135,6 @@
             return false;
           }
         });
-      },
-      handleBrandChange(val) {
-        let brandName = '';
-        for (let i = 0; i < this.brandOptions.length; i++) {
-          if (this.brandOptions[i].value === val) {
-            brandName = this.brandOptions[i].label;
-            break;
-          }
-        }
-        this.value.brandName = brandName;
       }
     }
   }
